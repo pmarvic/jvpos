c595c7566fe8af8abd260f7e97663a13fca9ae94
M4rv1c <ciaomarvic@gmail.com>
2016-04-23 22:47:34 -0430

    Update Projets

--- src/jvpos.java
+++ src/jvpos.java
@@ -1,5 +1,11 @@
+import config.*;
+
 /**
- * Created by MARVIC on 23/4/2016.
+ * Created by pmarvic on 23/4/2016.
  */
 public class jvpos {
+    public static void main(String arg[]) {
+        config runTerminal = new config();
+        runTerminal.readFile();
+    }
 }
